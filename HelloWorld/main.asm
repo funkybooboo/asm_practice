782719091118b22e631e730df8872baa6bfed9d0
nate <nate@Debian.myguest.virtualbox.org>
2025-01-13 01:19:48 +0000

    update hello world

--- HelloWorld/main.asm
+++ HelloWorld/main.asm
@@ -1,15 +1,18 @@
 section .data
-    msg db "Hello, world!", 0
-section .bss
+    msg db "Hello, World!", 0 ; Null-terminated string
+
 section .text
     global main
+
 main:
+    ; Write 'Hello, World!' to stdout
     mov     rax, 1      ; 1 = write
     mov     rdi, 1      ; 1 = to stdout
     mov     rsi, msg    ; string to display in rsi
-    mov     rdx, 13     ; length of the string without 0
+    mov     rdx, 13     ; length of the string without null-terminator
     syscall             ; display the string
+    
+    ; exit the program
     mov     rax, 60     ; 60 = exit
-    mov     rdi, 0      ; 0 = success exit code
+    xor     rdi, rdi    ; 0 = success exit code
     syscall             ; quit
-
