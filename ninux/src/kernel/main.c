d8d5deb7dc5ff9063ee972365eed3d61213141c6
Nate Stott <nate.stott@pm.me>
2025-03-16 13:16:37 -0400

    disk reset fuction

--- ninux/src/kernel/main.c
+++ ninux/src/kernel/main.c
@@ -1,7 +1,12 @@
 #include "stdio/stdio.h"
+#include "disk/disk.h"
 
 void _cdecl cstart_(){
-    puts("Ninux from C!\r\n");
-    printf("Formatted: %% %c %s\r\n", 'f', "Hello");
-    printf("%d,%i,%x,%p,%ld",1234, -2134, 0x1a, 0x3a, -1000000000l);
+    // puts("Ninux from C!\r\n");
+    // printf("Formatted: %% %c %s\r\n", 'f', "Hello");
+    // printf("%d,%i,%x,%p,%ld",1234, -2134, 0x1a, 0x3a, -1000000000l);
+
+    uint8_t error;
+    x86_Disk_Reset(0, &error);
+    printf("Error %d\r\n", error);
 }
