a0dae68bd35f66f1c0d8e5778407e123defaa071
Nate Stott <nate.stott@pm.me>
2025-03-16 12:33:44 -0400

    impl printf

--- ninux/src/kernel/print.asm
+++ ninux/src/kernel/print.asm
@@ -2,6 +2,35 @@ bits 16
 
 section _TEXT class=CODE
 
+global _x86_div64_32
+_x86_div64_32:
+    push bp
+    mov bp, sp
+
+    push bx
+
+    mov eax, [bp+8] ; upper 32 bits of dividends
+    mov ecx, [bp+12] ; divisor
+    xor edx, edx
+    div ecx
+
+    mov bx, [bp+16] ; upper 32 bits of the quotient
+    mov [bx+4], eax
+
+    mov eax, [bp+4] ; lower 32 bits of the dividend
+    div ecx
+
+    mov [bx], eax
+    mov bx, [bp+18]
+    mov [bx], edx
+
+    pop bx
+
+    mov sp, bp
+    pop bp
+
+    ret
+
 global _x86_Video_WriteCharTeletype
 _x86_Video_WriteCharTeletype:
     push bp              ; save the base pointer
