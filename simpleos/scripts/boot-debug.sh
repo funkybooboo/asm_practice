9f28dc069acf3c6a54da3e6a6416a77aa44c40aa
Nate Stott <nate.stott@pm.me>
2025-03-15 21:35:12 -0400

    read from disk in the bios

---
+++ simpleos/scripts/boot-debug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+qemu-system-i386 -boot c -m 256 -hda build/main.img -s -S
+
+# to connect to the debugger
+# `$ gdb`
+# `(gdb) target remote localhost:1234` 1234 is the port qemu's debugger is listening on
+# note jump over interrupts for easy debugging
