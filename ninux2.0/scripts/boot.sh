c16d14888890f72c5730610c4a3bffc042aad042
Nate Stott <nate.stott@pm.me>
2025-03-16 18:45:50 -0400

    impl gdt

--- ninux2.0/scripts/boot.sh
+++ ninux2.0/scripts/boot.sh
@@ -7,8 +7,12 @@ mkdir -p build/Ninux/boot/grub/
 
 gcc -m32 -fno-stack-protector -fno-builtin -c src/kernel.c -o build/kernel.o
 gcc -m32 -fno-stack-protector -fno-builtin -c src/vga.c -o build/vga.o
+gcc -m32 -fno-stack-protector -fno-builtin -c src/gdt.c -o build/gdt.o
+
 nasm -f elf32 src/bootloader/boot.asm -o build/boot.o
-ld -m elf_i386 -T src/linker.ld -o build/kernel build/boot.o build/kernel.o build/vga.o
+nasm -f elf32 src/gdt.asm -o build/gdts.o
+
+ld -m elf_i386 -T src/linker.ld -o build/kernel build/boot.o build/kernel.o build/vga.o build/gdt.o build/gdts.o
 
 cp build/kernel build/Ninux/boot/
 cp configs/grub.cfg build/Ninux/boot/grub/
--- ninux2.0/src/kernel.c
+++ ninux2.0/src/kernel.c
@@ -1,8 +1,12 @@
 #include "vga.h"
+#include "gdt.h"
 
 void kmain(void);
 
 void kmain(void) {
-    reset();
-    print("Hello, World!\r\n");
+    // reset();
+    // print("Hello, World!\r\n");
+
+    initGdt();
+    print("GDT is done!\r\n");
 }
